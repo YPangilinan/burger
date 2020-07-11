8483d428c43f5f4d1cdb7a2df3277d139add7d33
Yssabel Pangilinan <yssabelpangilinan@yahoo.com>
2020-07-10 18:20:52 -0700

    adding db and config folders

---
+++ config/orm.js
@@ -0,0 +1,27 @@
+var connection = require("../config/connection.js");
+
+
+const selectAll = () =>{
+
+}
+
+const insertOne = () =>{
+
+}
+
+const updateOne = () =>{
+
+}
+var orm = {
+    selectAll: function(){
+
+    },
+    insertOne: function(){
+
+    },
+    updateOne: function(){
+        
+    }
+}
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES("Jollibee Original Chicken Sandwich");
+
+INSERT INTO burgers (burger_name) VALUES("Jollibee Yum Burger");
+
+INSERT INTO burgers (burger_name) VALUES("Jollibee Hawaiian Yum Burger");
