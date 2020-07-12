e026039276044a5eeacf04436dcb91a1c391c645
Yssabel Pangilinan <yssabelpangilinan@yahoo.com>
2020-07-11 21:23:20 -0700

    adding server data to index.handlebars

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var PORT = process.env.PORT || 8080;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use(express.static(process.cwd() + "/public"));
 
 // Parse application body
 app.use(express.urlencoded({ extended: true }));
@@ -20,7 +20,7 @@ app.set("view engine", "handlebars");
 // Import routes and give the server access to them.
 var routes = require("./controllers/burger_controller.js");
 
-app.use(routes);
+app.use("/",routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
