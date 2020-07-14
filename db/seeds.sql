63cf511687f94e876919f5f08bb15ec24e81241d
Yssabel Pangilinan <yssabelpangilinan@yahoo.com>
2020-07-13 23:41:37 -0700

    further styling

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,11 @@
-INSERT INTO burgers (burger_name) VALUES("Jollibee Original Chicken Sandwich");
+INSERT INTO burgers (burger_name) VALUES("Happibee Original Chicken Sandwich");
 
-INSERT INTO burgers (burger_name) VALUES("Jollibee Yum Burger");
+INSERT INTO burgers (burger_name) VALUES("Happibee Yum Burger");
 
-INSERT INTO burgers (burger_name) VALUES("Jollibee Hawaiian Yum Burger");
+INSERT INTO burgers (burger_name) VALUES("Happibee Hawaiian Yum Burger");
+
+INSERT INTO burgers (burger_name, devoured) VALUES("Happibee Hawaiian Yum Burger", "1");
+
+INSERT INTO burgers (burger_name, devoured) VALUES("Happibee Classic Yum Yum", "1");
+
+INSERT INTO burgers (burger_name, devoured) VALUES("Happibee Chicken Yum Yum", "1");
